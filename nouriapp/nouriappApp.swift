572a890216a96bd7551b8febe7496adef1bca21b
het <hetpate384@gmail.com>
2026-04-04 22:51:50 -0400

    Initial Commit

---
+++ nouriapp/nouriappApp.swift
@@ -0,0 +1,17 @@
+//
+//  nouriappApp.swift
+//  nouriapp
+//
+//  Created by Het patel on 2026-04-04.
+//
+
+import SwiftUI
+
+@main
+struct nouriappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
